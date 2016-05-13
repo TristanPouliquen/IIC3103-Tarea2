f1d70c020cfe53924002dbc11b7e1dd442676277
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-13 19:03:56 -0300

    Route to fetch info of instagram posts by tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   root 'home#index'
+
+  post 'instagram/tag/buscar' => 'home#searchTag'
 end
